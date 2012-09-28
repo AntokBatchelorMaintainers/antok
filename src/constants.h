a4a8a7cbf5082492f6f050779a33d7f54cd39a67
Karl Bicker <kbicker@cern.ch>
2012-09-28 17:16:32 +0200

    antok: Cosmetics.
    Cleaned up the constants header: there were some old values for the particles
    masses in, which were used by Florian Haas and put in for the crosscheck. Moved
    them to the yaml config file (but commented them out). The other old constants
    have been removed as well.

--- src/constants.h
+++ src/constants.h
@@ -38,16 +38,7 @@ namespace antok {
 		friend class antok::Initializer;
 
 	};
-/*
-//	const double PION_MASS = 0.13957018;
-//	const double PROTON_MASS = 0.938272046;
-	const double PION_MASS = 0.139567;
-	const double PROTON_MASS = 0.9382723;
 
-	const double CHARGED_KAON_MASS = 0.493677;
-
-	const unsigned int N_PARTICLES = 5;
-*/
 }
 
 #endif
