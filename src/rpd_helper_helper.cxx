5cb5ec1d53ea804dc49a591ab535f07b95a1d723
Karl Bicker <kbicker@cern.ch>
2013-08-28 11:00:54 +0200

    antok: Update parameters in RpdHelperHelper.
    After fixing a problem in the fitRPDResolutions tool, update the fit
    parameters in the RpdHelperHelper class.

--- src/rpd_helper_helper.cxx
+++ src/rpd_helper_helper.cxx
@@ -24,42 +24,42 @@ antok::RpdHelperHelper::RpdHelperHelper() {
 	RPD_SLAB_PHI_ANGLES.push_back(0.194);
 
 	std::vector<double> leftSideLower;
-	leftSideLower.push_back(-0.0600894);
-	leftSideLower.push_back(-0.0211335);
-	leftSideLower.push_back(-0.0789747);
+	leftSideLower.push_back(-0.05928);
+	leftSideLower.push_back(-0.0204924);
+	leftSideLower.push_back(-0.0791312);
 	antok::FitParameters leftSideLowerParams = antok::FitParameters(leftSideLower);
 	std::vector<double> leftSideUpper;
-	leftSideUpper.push_back(0.0640313);
-	leftSideUpper.push_back(-0.00169554);
-	leftSideUpper.push_back(-0.0122071);
+	leftSideUpper.push_back(0.0642057);
+	leftSideUpper.push_back(-0.00171496);
+	leftSideUpper.push_back(-0.0122408);
 	antok::FitParameters leftSideUpperParams = antok::FitParameters(leftSideUpper);
 
 	_leftSlabLimitsParameters = std::pair<antok::FitParameters,
 	                                      antok::FitParameters>(leftSideLowerParams, leftSideUpperParams);
 
 	std::vector<double> middleLower;
-	middleLower.push_back(-0.131045);
-	middleLower.push_back(-0.0016428);
-	middleLower.push_back(-0.0122572);
+	middleLower.push_back(-0.130212);
+	middleLower.push_back(-0.00156496);
+	middleLower.push_back(-0.0124321);
 	antok::FitParameters middleLowerParams = antok::FitParameters(middleLower);
 	std::vector<double> middleUpper;
-	middleUpper.push_back(0.128913);
-	middleUpper.push_back(0.00157375);
-	middleUpper.push_back(-0.0125813);
+	middleUpper.push_back(0.129598);
+	middleUpper.push_back(0.00164405);
+	middleUpper.push_back(-0.0124098);
 	antok::FitParameters middleUpperParams = antok::FitParameters(middleUpper);
 
 	_middleSlabLimitsParameters = std::pair<antok::FitParameters,
 	                                        antok::FitParameters>(middleLowerParams, middleUpperParams);
 
 	std::vector<double> rightSideLower;
-	rightSideLower.push_back(-0.0646319);
-	rightSideLower.push_back(0.00171409);
-	rightSideLower.push_back(-0.0124187);
+	rightSideLower.push_back(-0.0642652);
+	rightSideLower.push_back(0.00158913);
+	rightSideLower.push_back(-0.011811);
 	antok::FitParameters rightSideLowerParams = antok::FitParameters(rightSideLower);
 	std::vector<double> rightSideUpper;
-	rightSideUpper.push_back(0.0587703);
-	rightSideUpper.push_back(0.0213982);
-	rightSideUpper.push_back(-0.0799849);
+	rightSideUpper.push_back(0.0587655);
+	rightSideUpper.push_back(0.0207241);
+	rightSideUpper.push_back(-0.0791871);
 	antok::FitParameters rightSideUpperParams = antok::FitParameters(rightSideUpper);
 
 	_rightSlabLimitsParameters = std::pair<antok::FitParameters,
@@ -72,13 +72,9 @@ std::pair<double, double> antok::RpdHelperHelper::getLimits(const double& rpdPhi
                                                             const TVector2& vertexXY)
 {
 
-	std::cout<<"before transform: phi="<<rpdPhi<<" x="<<vertexXY.X()<<" y="<<vertexXY.Y()<<std::endl;
-
 	std::pair<double, TVector2> transformedVariabes = rotateRpdAndVertex(rpdPhi, vertexXY);
 	const double& correctedProtonPhi = transformedVariabes.first;
 	const TVector2& correctedVertexXY = transformedVariabes.second;
-
-	std::cout<<"after transform: phi="<<correctedProtonPhi<<" x="<<correctedVertexXY.X()<<" y="<<correctedVertexXY.Y()<<std::endl;
 
 	std::pair<double, double> retval = std::pair<double, double>(0., 0.);
 
@@ -88,17 +84,14 @@ std::pair<double, double> antok::RpdHelperHelper::getLimits(const double& rpdPhi
 	for(; correctedProtonPhi > RPD_SLAB_PHI_ANGLES[slabType] + RPD_SLAB_STEP; ++slabType);
 	switch(slabType) {
 		case LEFTSLAB:
-			std::cout<<"returning stuff for left slab"<<std::endl;
 			retval.first = evaluatePlaneFunction(correctedVertexXY.X(), correctedVertexXY.Y(), _leftSlabLimitsParameters.first);
 			retval.second = evaluatePlaneFunction(correctedVertexXY.X(), correctedVertexXY.Y(), _leftSlabLimitsParameters.second);
 			break;
 		case MIDDLESLAB:
-			std::cout<<"returning stuff for middle slab"<<std::endl;
 			retval.first = evaluatePlaneFunction(correctedVertexXY.X(), correctedVertexXY.Y(), _middleSlabLimitsParameters.first);
 			retval.second = evaluatePlaneFunction(correctedVertexXY.X(), correctedVertexXY.Y(), _middleSlabLimitsParameters.second);
 			break;
 		case RIGHTSLAB:
-			std::cout<<"returning stuff for right slab"<<std::endl;
 			retval.first = evaluatePlaneFunction(correctedVertexXY.X(), correctedVertexXY.Y(), _rightSlabLimitsParameters.first);
 			retval.second = evaluatePlaneFunction(correctedVertexXY.X(), correctedVertexXY.Y(), _rightSlabLimitsParameters.second);
 			break;
