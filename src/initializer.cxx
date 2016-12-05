5c1f97c48156e73e58651c37ad8d58e67c6572a8
Christian Dreisbach <christian.dreisbach@cern.ch>
2016-12-05 14:54:45 +0100

    Fixed handling of plot names
    Plot names with invalid characters like "/" produce problems,
    because it is interpreted as an additional directory. Therefore
    only "normal" characters are allowed now: a-zA-Z0-9 and _ -

--- src/initializer.cxx
+++ src/initializer.cxx
@@ -716,6 +716,11 @@ bool antok::Initializer::initializePlotter() {
 		}
 		std::string plotName = antok::YAMLUtils::getString(plot["Name"]);
 
+        if( plotName.find_first_not_of("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890_-") != std::string::npos )
+        {
+            std::cerr<<"Invalid name for \"Plot\" \""<<plotName<<std::endl;
+            return false;
+        }
 		if((not((hasNodeKey(plot, "Variable") and hasNodeKey(plot, "LowerBound") and hasNodeKey(plot, "UpperBound")) or
 		       (hasNodeKey(plot, "Variables") and hasNodeKey(plot, "LowerBounds") and hasNodeKey(plot, "UpperBounds")))) or
 			not hasNodeKey(plot, "NBins"))
