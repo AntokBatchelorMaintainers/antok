1b6ca921ea4cffb8c15aa2f7cf89629706577aa1
Christian Dreisbach <christian.dreisbach@cern.ch>
2017-02-08 16:25:31 +0100

    Added new internal data type vector<TLorentzVector>

--- src/data.h
+++ src/data.h
@@ -47,6 +47,7 @@ namespace antok {
 
 		std::map<std::string, std::vector<int>* > intVectors;
 		std::map<std::string, std::vector<double>* > doubleVectors;
+		std::map<std::string, std::vector<TLorentzVector>* > lorentzVectorVectors;
 
 		std::map<std::string, TLorentzVector*> lorentzVectors;
 		std::map<std::string, TVector3> vectors;
