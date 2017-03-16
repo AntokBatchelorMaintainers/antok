0aca49c4161e4775aef27b73caa6d53e9003f8ae
Christian Dreisbach <christian.dreisbach@cern.ch>
2017-03-16 18:23:30 +0100

    Further work on example

--- tools/generateExampleFile.cxx
+++ tools/generateExampleFile.cxx
@@ -6,26 +6,25 @@
 
 const unsigned int numberOfEvents = 10000;
 
-std::vector<double> getGaussianData( double mean, double sigma )
+double getGaussianData( double mean, double sigma )
 {
-	std::vector<double> data;
-	data.resize(numberOfEvents);
-	for( unsigned int i = 0; i < numberOfEvents; i++ )
-	{
-		data[i] = gRandom->Gaus( mean, sigma );
-	}
-	return data;
+	return gRandom->Gaus( mean, sigma );
 }
 
-std::vector<double> getBoxData( double mean, double width )
+double getBoxData( double mean, double width )
 {
-	std::vector<double> data;
-	data.resize(numberOfEvents);
-	for( unsigned int i = 0; i < numberOfEvents; i++ )
+	double z;
+	do
 	{
-		data[i]= gRandom->Uniform( mean - width, mean +width );
+		z = gRandom->Exp(600);
 	}
-	return data;
+	while( z > mean + 2 * width );
+	return (mean - width * 0.5) + z;
+}
+
+unsigned int getIntegerData( unsigned int from, unsigned int count )
+{
+	return from + gRandom->Integer( from + count );
 }
 
 void generateExampleFile()
@@ -33,45 +32,91 @@ void generateExampleFile()
 	TFile* outFile = TFile::Open("example.root", "RECREATE");
 	outFile->cd();
 
-	std::vector<double> vertexX;
-	std::vector<double> vertexY;
-	std::vector<double> vertexZ;
+	unsigned int runNumber;
 
-	std::vector<double> beamMomentumX;
-	std::vector<double> beamMomentumY;
-	std::vector<double> beamMomentumZ;
+	double vertexX;
+	double vertexY;
+	double vertexZ;
 
-	std::vector<double> scatteredMomentumX;
-	std::vector<double> scatteredMomentumY;
-	std::vector<double> scatteredMomentumZ;
+	double beamGradientdXdZ;
+	double beamGradientdYdZ;
+
+	double recoilMomentumX;
+	double recoilMomentumY;
+	double recoilMomentumZ;
+	double recoilEnergy;
+
+	double scatteredMomentumX1;
+	double scatteredMomentumY1;
+	double scatteredMomentumZ1;
+
+	double scatteredMomentumX2;
+	double scatteredMomentumY2;
+	double scatteredMomentumZ2;
+
+	double scatteredMomentumX3;
+	double scatteredMomentumY3;
+	double scatteredMomentumZ3;
 
 	TTree* outTree = new TTree("exampleEvents", "exampleEvents");
-	outTree->Branch("VertexX", &vertexX );
-	outTree->Branch("VertexY", &vertexY );
-	outTree->Branch("VertexZ", &vertexZ );
+	outTree->Branch("RunNumber", &runNumber , "RunNumber/I" );
 
-	outTree->Branch("BeamMomentumX", &beamMomentumX,"beamMomentumX/D");
-	outTree->Branch("BeamMomentumY", &beamMomentumY,"beamMomentumY/D");
-	outTree->Branch("BeamMomentumZ", &beamMomentumZ,"beamMomentumZ/D");
+	outTree->Branch("VertexX", &vertexX, "VertexX/D" );
+	outTree->Branch("VertexY", &vertexY, "VertexY/D" );
+	outTree->Branch("VertexZ", &vertexZ, "VertexZ/D" );
 
-	outTree->Branch("ScatteredMomentumX", &scatteredMomentumX,"scatteredMomentumX/D");
-	outTree->Branch("ScatteredMomentumY", &scatteredMomentumY,"scatteredMomentumY/D");
-	outTree->Branch("ScatteredMomentumZ", &scatteredMomentumZ,"scatteredMomentumZ/D");
+	outTree->Branch("BeamGradientdXdZ", &beamGradientdXdZ, "BeamGradientdXdZ/D" );
+	outTree->Branch("BeamGradientdYdZ", &beamGradientdYdZ, "BeamGradientdYdZ/D" );
 
-	vertexX = getGaussianData( 0, 3.2    );
-	vertexY = getGaussianData( 0, 2.8    );
-	vertexZ = getBoxData     ( 0, 22.42  );
+	outTree->Branch("RecoilMomentumX", &recoilMomentumX, "RecoilMomentumX/D" );
+	outTree->Branch("RecoilMomentumY", &recoilMomentumY, "RecoilMomentumY/D" );
+	outTree->Branch("RecoilMomentumZ", &recoilMomentumZ, "RecoilMomentumZ/D" );
+	outTree->Branch("RecoilEnergy"   , &recoilEnergy   , "RecoilEnergy/D"    );
 
-	beamMomentumX = getGaussianData( 0.1, 0.5 );
-	beamMomentumY = getGaussianData( 0.2, 0.7 );
-	beamMomentumZ = getGaussianData( 200, 2.4 );
+	outTree->Branch("ScatteredMomentumX_1", &scatteredMomentumX1, "ScatteredMomentumX_1/D" );
+	outTree->Branch("ScatteredMomentumY_1", &scatteredMomentumY1, "ScatteredMomentumY_1/D" );
+	outTree->Branch("ScatteredMomentumZ_1", &scatteredMomentumZ1, "ScatteredMomentumZ_1/D" );
 
-	scatteredMomentumX = getGaussianData( 0.2, 0.4 );
-	scatteredMomentumY = getGaussianData( 0.4, 0.7 );
-	scatteredMomentumZ = getGaussianData( 198, 3.1 );
+	outTree->Branch("ScatteredMomentumX_2", &scatteredMomentumX2, "ScatteredMomentumX_2/D" );
+	outTree->Branch("ScatteredMomentumY_2", &scatteredMomentumY2, "ScatteredMomentumY_2/D" );
+	outTree->Branch("ScatteredMomentumZ_2", &scatteredMomentumZ2, "ScatteredMomentumZ_2/D" );
 
-	outTree->Fill();
+	outTree->Branch("ScatteredMomentumX_3", &scatteredMomentumX3, "ScatteredMomentumX_3/D" );
+	outTree->Branch("ScatteredMomentumY_3", &scatteredMomentumY3, "ScatteredMomentumY_3/D" );
+	outTree->Branch("ScatteredMomentumZ_3", &scatteredMomentumZ3, "ScatteredMomentumZ_3/D" );
+
+	for( unsigned int i = 0; i < numberOfEvents; ++i )
+	{
+		runNumber = getIntegerData( 10000, numberOfEvents );
+
+		vertexX = getGaussianData(  0.1,  3.32 );
+		vertexY = getGaussianData( -0.2,  2.82 );
+		vertexZ = getBoxData     (    0, 22.42 );
+
+		beamGradientdXdZ = getGaussianData( 0.001, 0.005 );
+		beamGradientdYdZ = getGaussianData( 0.002, 0.007 );
+
+		recoilMomentumX = getGaussianData( 0.1, 0.1 );
+		recoilMomentumY = getGaussianData( 0.2, 0.3 );
+		recoilMomentumZ = getGaussianData( 0.3, 0.2 );
+		recoilEnergy    = TMath::Abs(getGaussianData(   5, 0.1 ));
+
+		scatteredMomentumX1 = getGaussianData( 0.2, 0.4 );
+		scatteredMomentumY1 = getGaussianData( 0.4, 0.7 );
+		scatteredMomentumZ1 = getGaussianData( 75, 3.1  );
+
+		scatteredMomentumX2 = getGaussianData( 0.2, 0.4 );
+		scatteredMomentumY2 = getGaussianData( 0.4, 0.7 );
+		scatteredMomentumZ2 = getGaussianData( 80, 3.1  );
+
+		scatteredMomentumX3 = getGaussianData( 0.2, 0.4 );
+		scatteredMomentumY3 = getGaussianData( 0.6, 0.7 );
+		scatteredMomentumZ3 = getGaussianData(  55, 3.1 );
+
+		outTree->Fill();
+	}
 	outTree->Write();
+
 	outFile->Close();
 }
 
